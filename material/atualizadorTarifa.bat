d9ff575510676875a4a60ba6a2bb586cd57cbd9c
kaiosatiro <kaiosatiro@gmail.com>
2022-09-26 23:13:18 -0300

    removendo material de trabalho

--- material/atualizadorTarifa.bat
+++
@@ -1,9 +0,0 @@
-::Esse Script PODE ser executado sozinho
-set PGPASSWORD=postgres
-set cwd=%~dp0
-cd {PGWORKDIR}
-psql -U {user} -d {dbname} -h {host} --echo-errors-p {port} -c "DROP TABLE IF EXISTS tarifa_backup;CREATE TABLE tarifa_backup AS SELECT * FROM config_tarifa;BEGIN TRANSACTION;TRUNCATE config_tarifa;INSERT INTO config_tarifa  SELECT * FROM agendamento_config_tarifa;COMMIT;" >> %cwd%\ScriptAtualizaTarifaLOG.log 2>&1
-
-
-::Esse Script DEVE ser executado dentro da mesma pasta em que está o programa que o gerou.
-{SCRIPTDIR} --atualizar --host {host} --user {user} --port {port} --dbname {dbname}
